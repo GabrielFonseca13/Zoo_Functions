ad1e4cf5175d6e8b608d289347b8029b5aef437f
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-08-15 16:49:23 -0300

    Resolução do requisito2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
-}
+const getAnimalsOlderThan = (animal, age) => data.species
+  .find((specie) => specie.name === animal).residents
+  .every((specieAge) => (specieAge.age >= age));
+
+console.log(getAnimalsOlderThan('lions', 10));
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,17 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  const arrayDeRetorno = [];
+  if (ids.length === 0) return arrayDeRetorno;
+  if (ids.length === 1) {
+    for (let index = 0; index < data.species.length; index += 1){
+      const specie = data.species[index];
+      if (specie.id === ids[0]){
+        return specie.name;
+      }
+    }
+  }
 }
-
+console.log(specie.name);
 module.exports = getSpeciesByIds;
