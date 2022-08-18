eefe25e2d284358062c789b9df746c36f41f3b36
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-08-17 23:59:00 -0300

    resolução do requisito 9

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,25 @@
+const animals = require('./getSpeciesByIds');
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-
+function getEmployeesCoverage(person = { name: '', id: '' }) {
+  const employee = data.employees.filter((elemento) => {
+    const fullName = `${elemento.firstName} ${elemento.lastName}`;
+    return fullName.includes(person.name) || elemento.id.includes(person.id);
+  });
+  if (employee.length === 0) {
+    throw new Error('Informações inválidas');
+  }
+  const arrayResult = [];
+  for (let index = 0; index < employee.length; index += 1) {
+    arrayResult.push({ id: employee[index].id,
+      fullName: `${employee[index].firstName} ${employee[index].lastName}`,
+      species: animals(...employee[index].responsibleFor).map((elemento) => elemento.name),
+      locations: animals(...employee[index].responsibleFor).map((elemento) => elemento.location) });
+  }
+  if (person.name === '' && person.id === '') {
+    return arrayResult;
+  }
+  return arrayResult[0];
 }
 
 module.exports = getEmployeesCoverage;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,42 @@
 const data = require('../data/zoo_data');
 
+const available = (animal) => data.species.find((e) => e.name === animal).availability;
+
+const availableHours = (day) => {
+  if (day === 'Monday') {
+    return { Monday: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' } };
+  }
+  const hour = {
+    [day]: {
+      officeHour: `Open from ${data.hours[day].open}am until ${data.hours[day].close}pm`,
+      exhibition: data.species
+        .filter((e) => e.availability
+          .includes(day))
+        .map((b) => b.name),
+    },
+  };
+  return hour;
+};
+
+const allDays = () => Object.keys(data.hours).reduce((acc, currentValue) => {
+  Object.assign(acc, availableHours(currentValue));
+  return acc;
+}, {});
+
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  const verify = Object.keys(data.hours);
+  const verifyAnimal = data.species.some((animal) => animal.name === scheduleTarget);
+
+  if (verify.includes(scheduleTarget)) {
+    return availableHours(scheduleTarget);
+  }
+  if (scheduleTarget === undefined) {
+    return allDays();
+  }
+  if (!verifyAnimal) {
+    return allDays();
+  }
+  return available(scheduleTarget);
 }
 
 module.exports = getSchedule;
